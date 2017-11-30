9f4ffeae11c4af148e5e122f3027077d5c7c2218
teknoplop <teknoplop@users.noreply.github.com>
2017-11-30 18:10:53 +0000

    add boost date_time .so dependancy

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include <boost/date_time.hpp>
+
 int main( int argc, char** argv )
 {
-	std::cout << "Hello"
+	std::cout << "Hello at"
+		<< boost::posix_time::to_iso_string(boost::posix_time::second_clock::local_time())
 		<< std::endl;
 
 	return 0;
