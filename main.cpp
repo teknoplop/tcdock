9a96bc8b85fb3c28284cd6ff24cf5f07ea554564
teknoplop <teknoplop@users.noreply.github.com>
2017-11-30 18:36:08 +0000

    add space before timestamp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main( int argc, char** argv )
 {
-	std::cout << "Hello at"
+	std::cout << "Hello at "
 		<< boost::posix_time::to_iso_string(boost::posix_time::second_clock::local_time())
 		<< std::endl;
 
